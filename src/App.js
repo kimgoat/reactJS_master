14d2407ef7969a32aef81f738b2b1e4590805144
kimgoat <kimgoatlove@gmail.com>
2024-03-08 13:50:37 +0900

    #2.4 Animations and Pesudo Selctors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Container = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true })`
-  background-color: pink;
-`;
+const rotateAnimation = keyframes`
+  /* from{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  to{
+    transform: rotate(360deg);
+    border-radius: 100px;
+  } */
 
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 100px;
+    background-color: black;
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 200px;
+  background-color: pink;
+  animation: ${rotateAnimation} 1s linear infinite;
+
+  span{
+    font-size: 100px;
+    &:hover{
+      font-size: 200px;
+    }
+  }
+/* 
+  span:hover{
+    font-size: 200px;
+  } */
+`;
 
 function App() {
   return (
-    <Container>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Container>
+    <Wrapper>
+      <Box >
+        <span>😆</span>
+      </Box>
+    </Wrapper>
   );
 }
 
