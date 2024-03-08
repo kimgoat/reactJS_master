197cb981519da7b308e8e92157e38e07d227163f
kimgoat <kimgoatlove@gmail.com>
2024-03-08 13:32:01 +0900

    #2.3 'As'를 사용하여 HTML 태그 설정하기

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-display: flexbox;
+  display: flex;
 `;
 
-const BoxContainer = styled(Container)`
-display: flex;
+const Btn = styled.button`
+  color: pink;
+  background-color: black;
+  border: 0;
+  border-radius: 20px;
+  width: 50px;
 `;
 
-const Box = styled.div`
-  width: 200px;
-  height: 300px;
-  background-color: ${(props) => props.bgColor};
-`;
-
-const ButtonContainer = styled(Container)`
-`;
-
-const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.$primary ? "#BF4F74" : "white"};
-  color: ${props => props.$primary ? "white" : "#BF4F74"};
-
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid #BF4F74;
-  border-radius: 3px;
-`;
 
 function App() {
   return (
     <Container>
-      <BoxContainer>
-        <Box bgColor="pink" />
-        <Box bgColor="red" />
-      </BoxContainer>
-      <ButtonContainer>
-        <Button>Normal</Button>
-        <Button $primary>Primary</Button>
-      </ButtonContainer>
+      <Btn>Log in</Btn>
+      <Btn as="a" href="https://styled-components.com/docs/basics">Log in</Btn>
     </Container>
   );
 }
