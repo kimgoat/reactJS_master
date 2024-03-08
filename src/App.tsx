d55e4a9dfe48193cff7fe8ae56f53de7d2a0e5a8
kimgoat <kimgoatlove@gmail.com>
2024-03-08 18:14:42 +0900

    [build] 사용 안할 예제 코드 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import styled from "styled-components";
-import Cercle from "./Cercle";
-import PropsEx from "./PropsEx";
 
 function App() {
-  return (
-    <div>
-      <Cercle bgColor="pink" borderColor="black" />
-      <Cercle bgColor="black" />
-      <Cercle bgColor="skyblue" text="hello?" />
-      <PropsEx />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
