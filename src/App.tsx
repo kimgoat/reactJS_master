66cefe83322b5d388f4212b174c2c6d8cb8cbed8
kimgoat <kimgoatlove@gmail.com>
2024-03-08 18:24:51 +0900

    #3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("");
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button> Log in</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
