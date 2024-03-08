2545a4a052624588344b5c5b461e6e148b237fa9
kimgoat <kimgoatlove@gmail.com>
2024-03-08 14:50:39 +0900

    ThemeProvider를 사용하여 Theme 적용하기

--- src/App.js
+++ src/App.js
@@ -4,59 +4,21 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vmax;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${props => props.theme.backgroundColor};
 `;
 
-const rotateAnimation = keyframes`
-  /* from{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  to{
-    transform: rotate(360deg);
-    border-radius: 100px;
-  } */
-
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);
-    border-radius: 100px;
-    background-color: black;
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`
-const Emoji = styled.div`
- font-size: 100px;
-`
-
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 200px;
-  width: 200px;
-  background-color: pink;
-  animation: ${rotateAnimation} 1s linear infinite;
-
-  ${Emoji}{
-    &:hover{
-      font-size: 200px;
-    }
-  }
+const Text = styled.span`
+  color: ${props => props.theme.textColor};
 `;
+
+
 
 function App() {
   return (
     <Wrapper>
-      <Box >
-        <Emoji>😆</Emoji>
-      </Box>
+      <Text>Hello</Text>
     </Wrapper>
   );
 }
