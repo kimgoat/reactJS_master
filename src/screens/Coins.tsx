ffd72d42fe508afb9ad13a83f2dd2a669f6a5262
kimgoat <kimgoatlove@gmail.com>
2024-03-09 19:11:43 +0900

    #5.1 Styles

--- src/screens/Coins.tsx
+++ src/screens/Coins.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+
 export default function Coins() {
   return (
     <>
-      <h1>Coins</h1>
+      <Title>Coins</Title>
     </>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#192a56",
+  textColor: "#dcdde1",
+  accentColor: "#e1b12c",
 };
