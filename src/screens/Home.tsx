745343a3f7e3eaae0eadf3eef8c109343722414b
kimgoat <kimgoatlove@gmail.com>
2024-03-09 18:15:47 +0900

    #4.8 useSearchParams & Extras

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 export default function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  console.log(readSearchParams.has("geo")); // url에 "geo"가 포함되는지 여부 (return boolean)
+  console.log(readSearchParams.get("geo")); // url에 "geo"가 가지는 값을 가져오기 (return string)
+  setTimeout(() => {
+    // url을 변경하기
+    setSearchParams({
+      day: "today",
+      tommorrow: "10",
+    });
+  }, 3000);
   return (
     <>
       <h1>Users</h1>
