8ed4366ee4de8029286e33866963d3d99bb12919
kimgoat <kimgoatlove@gmail.com>
2024-03-09 19:42:23 +0900

    #5.2 Home part 1

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#192a56",
   textColor: "#dcdde1",
-  accentColor: "#e1b12c",
+  accentColor: "#c23616",
 };
