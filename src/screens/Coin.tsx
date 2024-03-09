d2608500aefb46fdfedb29a2d18b39e00c85a3a2
kimgoat <kimgoatlove@gmail.com>
2024-03-09 22:19:08 +0900

    #5.6 Data Types

--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -30,18 +30,68 @@ const Loader = styled.span`
 //   name: string;
 // }
 
-interface PriceInterface {
-  price: number;
-  ath_price: number;
-  ath_date: string;
+interface IInfoData {
+  // mac (option + shift + i) window (alt + shift + i) 단축키
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
 }
 
 export default function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   const { state } = useLocation();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
   useEffect(() => {
     (async () => {
@@ -52,6 +102,7 @@ export default function Coin() {
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
+
       setInfo(infoData);
       setPriceInfo(priceData);
       setLoading(false);
@@ -63,7 +114,11 @@ export default function Coin() {
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <h1>null</h1>}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <h1> {priceInfo?.quotes.USD.price}</h1>
+      )}
     </Container>
   );
 }
