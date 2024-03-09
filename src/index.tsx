e26a5c325bacc0882dfe09e29de8c32df6583d5a
kimgoat <kimgoatlove@gmail.com>
2024-03-09 15:12:24 +0900

    [build] 필요없는 파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
 
 const element = document.getElementById("root");
 const root = createRoot(element as Element);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
