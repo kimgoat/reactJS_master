c949dc58688f6fbdd12c5f505d7a170ed3913d25
kimgoat <kimgoatlove@gmail.com>
2024-03-10 14:42:05 +0900

    #5.9 React Query Part 1

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const element = document.getElementById("root");
 const root = createRoot(element as Element);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
