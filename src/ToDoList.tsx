969914dd3bef75361901555dae0a9f4cafd1939a
kimgoat <kimgoatlove@gmail.com>
2024-03-12 20:17:08 +0900

    [chore] 필요없는 코드 삭제

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,34 +1,4 @@
-import { useEffect, useState } from "react";
-import { set, useForm } from "react-hook-form";
-
-/*
-export default function ToDoList() {
-  const [todo, setTodo] = useState("");
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setTodo(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(todo);
-  };
-
-  return (
-    <>
-      <h1>To Do List</h1>
-
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="Write a to do" />
-        <button> Add</button>
-      </form>
-    </>
-  );
-}
-*/
+import { useForm } from "react-hook-form";
 
 type IFormData = {
   errors: {
@@ -49,7 +19,6 @@ type IFormData = {
 export default function ToDoList() {
   const {
     register,
-    watch,
     handleSubmit,
     formState: { errors },
     setError,
