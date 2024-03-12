ec0e7cae149659b2b01e680f3b48b8ba96e3ed74
kimgoat <kimgoatlove@gmail.com>
2024-03-12 21:09:11 +0900

    #6.13 Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,35 @@
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-export default function ToDo({ text }: IToDo) {
+export default function ToDo({ text, category, id }: IToDo) {
+  //   const handleCategory = (newCategory: IToDo["category"]) => {
+  //     console.log(newCategory);
+  //   };
+  const setToDos = useSetRecoilState(toDoState);
+  const handleCategory = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <>
       <li>
         <span>{text} </span>
-        <button>To Do</button>
-        <button>Doing</button>
-        <button>Done</button>
+        {category !== "TO_DO" && (
+          <button name="TO_DO" onClick={handleCategory}>
+            To Do
+          </button>
+        )}
+        {category !== "DOING" && (
+          <button name="DOING" onClick={handleCategory}>
+            Doing
+          </button>
+        )}
+        {category !== "DONE" && (
+          <button name="DONE" onClick={handleCategory}>
+            Done
+          </button>
+        )}
       </li>
     </>
   );
