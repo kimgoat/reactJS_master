7a461b171564e530e55d4c6e6d1c18b749ee22da
kimgoat <kimgoatlove@gmail.com>
2024-03-12 19:05:26 +0900

    #6.7 Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -31,19 +31,58 @@ export default function ToDoList() {
 */
 
 export default function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, watch, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    // 필수로 handleSubmit에 넘겨줘야 함
+    console.log(data);
+  };
 
+  const onInValid = (data: any) => {
+    // 필수는 아님
+    console.log(formState.errors);
+  };
   return (
     <>
       <h1>To Do List</h1>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("first_name")} placeholder="First Name" />
-        <input {...register("last_name")} placeholder="Last Name" />
-        <input {...register("user_name")} placeholder="User Name" />
-        <input {...register("id")} placeholder="ID" />
-        <input {...register("password")} placeholder="Password" />
+      <form onSubmit={handleSubmit(onValid, onInValid)}>
+        <input
+          {...register("email", {
+            required: true,
+            pattern: {
+              value: /[A-Za-z]{3}/,
+              message: "알파벳이 3글자 이상 포함되어야 함(대소문자 구분 x)",
+            },
+          })}
+          placeholder="Email"
+        />
+        <input
+          {...register("first_name", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("last_name", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("user_name", { required: true })}
+          placeholder="User Name"
+        />
+        <input
+          {...register("id", {
+            required: { value: true, message: "ID를 입력해주세요." },
+          })}
+          placeholder="ID"
+        />
+        <input
+          {...register("password", {
+            required: true,
+            minLength: {
+              value: 10,
+              message: "password는 10글자 이상이어야 합니다.",
+            },
+          })}
+          placeholder="Password"
+        />
         <button> Add</button>
       </form>
     </>
