3b1659772ca146416c076e487c304a9683bccb60
kimgoat <kimgoatlove@gmail.com>
2024-03-12 22:00:42 +0900

    #6.16 Selector Part 1

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,23 +1,39 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import ToDo from "./ToDo";
 
 export default function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <>
       <h1>To Do List</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
-          // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
+      <hr />
+
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((doing) => (
+          <ToDo key={doing.id} {...doing} />
+        ))}
+      </ul>
+      <hr />
+
+      <h2>Done</h2>
+      <ul>
+        {done.map((done) => (
+          <ToDo key={done.id} {...done} />
+        ))}
+      </ul>
+      <hr />
     </>
   );
 }
