8c04925c12842a2a791e24d3ebb63805319ae8dc
kimgoat <kimgoatlove@gmail.com>
2024-03-14 15:43:27 +0900

    #7.0 Get Selectors

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import ToDoList from "./components/ToDoList";
+import Ex1 from "./practice/Ex1";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -62,7 +63,8 @@ function Root() {
   return (
     <>
       <GlobalStyle />
-      <ToDoList />
+      {/* <ToDoList /> */}
+      <Ex1 />
     </>
   );
 }
---
+++ src/practice/Ex1.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { minuteState, hourSelector } from "./atoms1";
+
+export default function Ex1() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
+}
