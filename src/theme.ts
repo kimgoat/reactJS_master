53814ff6172d3a23b1b2b4588760bb8f814ca131
kimgoat <kimgoatlove@gmail.com>
2024-03-14 16:56:43 +0900

    #7.4 Styles and Placeholders

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,10 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#161616",
-  textColor: "#dcdde1",
+  // bgColor: "#161616",
+  textColor: "black",
   accentColor: "#c23616",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
