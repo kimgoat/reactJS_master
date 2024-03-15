61ba46a72373ec54c7fcfbc4a48af225987f60b1
kimgoat <kimgoatlove@gmail.com>
2024-03-15 18:22:46 +0900

    #8.9 MotionValues Part 3: useScroll()을 사용하여 스크롤하는 경우 요소의 값 변경하기

--- src/animation/App.tsx
+++ src/animation/App.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -21,15 +20,21 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const scaleX = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-  const scaleY = useTransform(y, [300, 0, -300], [2, 1, 0.1]);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(218, 52, 255), rgb(255, 166, 228))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper>
-      <Box style={{ x, scaleX }} drag="x" dragSnapToOrigin />
-      <Box style={{ y, scaleY }} drag="y" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
