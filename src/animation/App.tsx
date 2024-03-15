54b09803861ed7357f422d8594e44be8f12c2ebd
kimgoat <kimgoatlove@gmail.com>
2024-03-15 17:23:52 +0900

    #8.3 Variants Part 1

--- src/animation/App.tsx
+++ src/animation/App.tsx
@@ -10,12 +10,18 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  margin: 10px;
   width: 200px;
   height: 200px;
   background-color: white;
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
 
 function App() {
   return (
@@ -25,7 +31,12 @@ function App() {
         initial={{ scale: 0 }}
         animate={{ scale: 1, rotateZ: 360 }}
       />
-      <motion.div animate={{ x: 50 }}>hello✋</motion.div>
+      <Box
+        style={{ backgroundColor: "pink" }}
+        variants={myVars}
+        initial="start"
+        animate="end"
+      />
     </Wrapper>
   );
 }
