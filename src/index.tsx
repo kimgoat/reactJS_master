b23464e4e5918ecece3f433deac244671a3cc02a
kimgoat <kimgoatlove@gmail.com>
2024-03-16 16:33:03 +0900

    #7.5 Home Screen part 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import router from "./Router";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
+const client = new QueryClient();
 const element = document.getElementById("root");
 const root = createRoot(element as Element);
 root.render(
   // <React.StrictMode>
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </RecoilRoot>
   // </React.StrictMode>
 );
---
+++ src/netflix/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = process.env.REACT_APP_THEMOVIEDB_APP_KEY;
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
