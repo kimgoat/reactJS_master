17cf7367d2278143bc43635ee99df62b118bbe91
kimgoat <kimgoatlove@gmail.com>
2024-03-16 17:46:27 +0900

    #9.7 Slider part 1

--- src/netflix/routes/Home.tsx
+++ src/netflix/routes/Home.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "react-query";
 import { IGetMoviesResult, getMovies } from "../api";
+import { motion, AnimatePresence, Variants } from "framer-motion";
 import styled from "styled-components";
 import { makeImgPath } from "../utils";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -35,21 +37,79 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ itemPhoto: string }>`
+  background-color: whitesmoke;
+  height: 250px;
+  font-size: 20px;
+  background-image: url(${(props) => props.itemPhoto});
+  background-size: cover;
+`;
+
+const rowVariants: Variants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => setIndex((prev) => prev + 1);
   return (
     <Wrapper>
       {isLoading ? (
         <Loader>is Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results.map((i) => (
+                  <Box
+                    key={i.id}
+                    itemPhoto={makeImgPath(i.poster_path || "")}
+                  ></Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
